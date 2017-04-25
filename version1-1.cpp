180973ab6c5c61831c6dec44e89cd41cbd596f49
lmkoida <l.koida@tu-braunschweig.de>
2017-04-25 19:57:24 +0200

    Fakultätrechner erstellt

--- version1-1.cpp
+++ version1-1.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main ()
 {
+    int a;
+    int b=1;
     
+    cout << endl;
+    cout << "Gib eine Zahl ein: ";
+    cin >> a;
+        
+    do
+    {
+        b=b*a;
+        a=a-1;
+    }
+    while(a>0);
+    
+    cout << endl;
+    cout << "Die Fakultät ist " << b << "." << endl;
+    cout << endl;
+    return 0;
 }
